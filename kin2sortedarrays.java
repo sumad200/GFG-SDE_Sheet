6aac86f225e8191788bfd2a92f035b1364270e4b
sumad200 <92201689+sumad200@users.noreply.github.com>
2022-05-31 15:43:44 +0530

    Create kin2sortedarrays.java

---
+++ kin2sortedarrays.java
@@ -0,0 +1,75 @@
+/*
+
+K th element in 2 sorted arrays
+
+
+
+*/
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        if(m < n){
+            kthElement(arr2,arr1,m,n,k);
+        }
+        
+        int low = Math.max(0,k-m);
+        int hi  = Math.min(k,n);
+        
+        while(low <= hi){
+            
+            int p1 = (low+hi) >> 1;
+            int p2 = k-p1;
+            
+            int l1; 
+            
+            if(p1 > 0){ 
+            l1 = arr1[p1-1];
+            }
+            else{
+            l1 = Integer.MIN_VALUE;
+            }
+            
+            
+            int l2; 
+            
+            if(p2 > 0){
+            l2 = arr2[p2-1];
+            }
+            else{
+            l2 = Integer.MIN_VALUE;
+            }  
+            
+            int r1;
+            if (p1 < n) { 
+                r1 = arr1[p1];
+            }
+            else{
+                r1 = Integer.MAX_VALUE;
+            }
+            int r2; 
+            if (p2 < m) {
+                r2 = arr2[p2] ;
+            }
+            else{
+               r2 = Integer.MAX_VALUE;  
+            } 
+            
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1,l2);
+            }
+            
+            else if( l2 > r1 ){
+                low = p1 + 1;
+            }
+            
+            else{
+                hi = p1 - 1;
+            }
+            
+            
+        }
+         
+     return -1;   
+    }
+}

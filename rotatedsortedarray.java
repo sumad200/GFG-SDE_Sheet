f132f824c9812396ef4f6240b19ff4b9408a11f7
sumad200 <92201689+sumad200@users.noreply.github.com>
2022-06-15 00:00:33 +0530

    Update rotatedsortedarray.java

--- rotatedsortedarray.java
+++ rotatedsortedarray.java
@@ -5,6 +5,11 @@
 
 for every point in given array either low to mid or mid to high is sorted
 
+find which of the above is true for the current mid since this part is sorted if target lie in this region 
+
+it must be in the interval of that half, if not then pass to next half
+
+logic partially derived from neetcode video
 
 
 */
@@ -20,7 +25,7 @@ class Solution {
             if(nums[mid] == target) return mid;
             
             if(nums[mid]>=nums[l]){
-                if(target>=nums[l] && target<nums[mid])
+                if(target>=nums[l] && target<=nums[mid])
                     h = mid-1;
                 else
                     l = mid+1;
@@ -28,7 +33,7 @@ class Solution {
             }
             else if(nums[mid]<=nums[nums.length-1]){
                 
-                if(target>nums[mid] && target<=nums[nums.length-1])
+                if(target>=nums[mid] && target<=nums[nums.length-1])
                     l = mid+1;
                 else
                     h = mid-1;
